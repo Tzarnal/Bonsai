e1e0229d3bafd8ae7ab77eb2ab2e6c9e867f01fe
Stefan van Oudenaarden <s.oudenaarden@gmail.com>
2021-01-19 14:42:05 +0100

    Dutch Name Generator

---
+++ BonsaiGenerators/Content/Real Names/DutchNames.cs
@@ -0,0 +1,43 @@
+﻿using BonsaiGenerators;
+using BonsaiGenerators.Tables.Names.Real.Dutch;
+
+namespace BonsaiGenerators.Content
+{
+    public class DutchNames : RandomGenerator
+    {
+        private static readonly RandomGenerator FamilyName = new DutchFamilyNames();
+        private static readonly RandomGenerator MaleGivenName = new DutchMaleGiven();
+        private static readonly RandomGenerator FemaleGivenName = new DutchFemaleGiven();
+
+        private static readonly RandomGenerator GivenName = new RandomTableMixed
+        {
+            MaleGivenName,
+            FemaleGivenName
+        };
+
+        public override string ToString()
+        {
+            return $"{GivenName} {FamilyName}";
+        }
+
+        public static string FirstNameFemale()
+        {
+            return FemaleGivenName.Next();
+        }
+
+        public static string FirstNameMale()
+        {
+            return MaleGivenName.Next();
+        }
+
+        public static string FirstName()
+        {
+            return GivenName.Next();
+        }
+
+        public static string LastName()
+        {
+            return FamilyName.Next();
+        }
+    }
+}
