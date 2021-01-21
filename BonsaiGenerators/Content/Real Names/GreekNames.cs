15022416c2252ea2db9e1801881f5d8c3fd7d6bc
Stefan van Oudenaarden <s.oudenaarden@gmail.com>
2021-01-21 14:26:16 +0100

    Generator: Greek Names

---
+++ BonsaiGenerators/Content/Real Names/GreekNames.cs
@@ -0,0 +1,38 @@
+﻿using BonsaiGenerators.Tables.Names.Real.Greek;
+
+namespace BonsaiGenerators.Content
+{
+    public class GreekNames : RandomNameGenerator
+    {
+        private static readonly GreekFemaleFamily FemaleFamilyName = new();
+        private static readonly GreekMaleFamily MaleFamilyName = new();
+        private static readonly GreekMaleGiven MaleGivenName = new();
+        private static readonly GreekFemaleGiven FemaleGivenName = new();
+
+        public override string FemaleFullName()
+        {
+            return $"{FemaleGivenName} {FemaleFamilyName}";
+        }
+
+        public override string MaleFullName()
+        {
+            return $"{MaleGivenName} {MaleFamilyName}";
+        }
+
+        public override string FullName()
+        {
+            return ToString();
+        }
+
+        public override string ToString()
+        {
+            var coin = Genie.Instance.Next(0, 2);
+            if (coin == 1)
+            {
+                return MaleFullName();
+            }
+
+            return FemaleFullName();
+        }
+    }
+}
