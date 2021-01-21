eae19d10dddc21b8add5a30ed418313b5b9ad711
Stefan van Oudenaarden <s.oudenaarden@gmail.com>
2021-01-21 13:50:41 +0100

    Generator: North American Names

---
+++ BonsaiGenerators/Content/Real Names/AmericanNames.cs
@@ -0,0 +1,37 @@
+﻿using BonsaiGenerators.Tables.Names.Real.American;
+
+namespace BonsaiGenerators.Content
+{
+    public class AmericanNames : RandomNameGenerator
+    {
+        private static readonly RandomGenerator FamilyName = new AmericanFamily();
+        private static readonly RandomGenerator MaleGivenName = new AmericanMaleGiven();
+        private static readonly RandomGenerator FemaleGivenName = new AmericanFemaleGiven();
+
+        private static readonly RandomGenerator GivenName = new RandomTableMixed
+        {
+            MaleGivenName,
+            FemaleGivenName
+        };
+
+        public override string FullName()
+        {
+            return ToString();
+        }
+
+        public override string FemaleFullName()
+        {
+            return $"{FemaleGivenName} {FamilyName}";
+        }
+
+        public override string MaleFullName()
+        {
+            return $"{MaleGivenName} {FamilyName}";
+        }
+
+        public override string ToString()
+        {
+            return $"{GivenName} {FamilyName}";
+        }
+    }
+}
