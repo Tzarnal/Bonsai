b0f3e2ed2af63eba9851f5dec599893ab5428d0a
Stefan van Oudenaarden <s.oudenaarden@gmail.com>
2021-01-21 14:48:24 +0100

    Generator: Random Real Names

---
+++ BonsaiGenerators/Content/Real Names/AllNames.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace BonsaiGenerators.Content
+{
+    public class AllNames : RandomNameGenerator
+    {
+        private static readonly List<RandomNameGenerator> NameGenerators = new List<RandomNameGenerator>()
+        {
+            new AmericanNames(),
+            new ChineseNames(),
+            new DutchNames(),
+            new FrenchNames(),
+            new GreekNames(),
+        };
+
+        public override string FemaleFullName()
+        {
+            return NameGenerators.RandomElement().FemaleFullName();
+        }
+
+        public override string MaleFullName()
+        {
+            return NameGenerators.RandomElement().MaleFullName();
+        }
+
+        public override string FullName()
+        {
+            return NameGenerators.RandomElement().FullName();
+        }
+
+        public override string ToString()
+        {
+            return FullName();
+        }
+    }
+}
