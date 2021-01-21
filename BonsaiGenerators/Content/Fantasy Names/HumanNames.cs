13aa75b35ede186fe9e5f4b201e295d61e938e2d
Stefan van Oudenaarden <s.oudenaarden@gmail.com>
2021-01-21 15:22:45 +0100

    Generator: Fantasy Humans

---
+++ BonsaiGenerators/Content/Fantasy Names/HumanNames.cs
@@ -0,0 +1,37 @@
+﻿using BonsaiGenerators.Tables.Names.Fantasy.Human;
+
+namespace BonsaiGenerators.Content
+{
+    public class HumanNames : RandomNameGenerator
+    {
+        private static readonly HumanFamily FamilyName = new();
+        private static readonly HumanMaleGiven MaleGivenName = new();
+        private static readonly HumanFemaleGiven FemaleGivenName = new();
+
+        private static readonly RandomGenerator GivenName = new RandomTableMixed
+        {
+            MaleGivenName,
+            FemaleGivenName
+        };
+
+        public override string FullName()
+        {
+            return ToString();
+        }
+
+        public override string FemaleFullName()
+        {
+            return $"{FemaleGivenName} {FamilyName}";
+        }
+
+        public override string MaleFullName()
+        {
+            return $"{MaleGivenName} {FamilyName}";
+        }
+
+        public override string ToString()
+        {
+            return $"{GivenName} {FamilyName}";
+        }
+    }
+}
