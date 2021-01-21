b3f78628f94987bf38909e95ca099901523434d7
Stefan van Oudenaarden <s.oudenaarden@gmail.com>
2021-01-21 14:14:31 +0100

    Generator: French Names

---
+++ BonsaiGenerators/Content/Real Names/FrenchNames.cs
@@ -0,0 +1,37 @@
+﻿using BonsaiGenerators.Tables.Names.Real.French;
+
+namespace BonsaiGenerators.Content
+{
+    public class FrenchNames : RandomNameGenerator
+    {
+        private static readonly FrenchFamily FamilyName = new();
+        private static readonly FrenchMaleGiven MaleGivenName = new();
+        private static readonly FrenchFemaleGiven FemaleGivenName = new();
+
+        private static readonly RandomGenerator GivenName = new RandomTableMixed
+        {
+            MaleGivenName,
+            FemaleGivenName
+        };
+
+        public override string FullName()
+        {
+            return ToString();
+        }
+
+        public override string FemaleFullName()
+        {
+            return $"{FemaleGivenName} {FamilyName}";
+        }
+
+        public override string MaleFullName()
+        {
+            return $"{MaleGivenName} {FamilyName}";
+        }
+
+        public override string ToString()
+        {
+            return $"{GivenName} {FamilyName}";
+        }
+    }
+}
