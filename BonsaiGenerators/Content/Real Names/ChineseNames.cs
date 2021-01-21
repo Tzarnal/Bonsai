1fb36f7201e2c9f072f874688ca076d3b3f948ee
Stefan van Oudenaarden <s.oudenaarden@gmail.com>
2021-01-21 13:58:32 +0100

    Generator: Chinese Names

---
+++ BonsaiGenerators/Content/Real Names/ChineseNames.cs
@@ -0,0 +1,37 @@
+﻿using BonsaiGenerators.Tables.Names.Real.Chinese;
+
+namespace BonsaiGenerators.Content
+{
+    public class ChineseNames : RandomNameGenerator
+    {
+        private static readonly ChineseFamily FamilyName = new();
+        private static readonly ChineseMaleGiven MaleGivenName = new();
+        private static readonly ChineseFemaleGiven FemaleGivenName = new();
+
+        private static readonly RandomGenerator GivenName = new RandomTableMixed
+        {
+            MaleGivenName,
+            FemaleGivenName
+        };
+
+        public override string FullName()
+        {
+            return ToString();
+        }
+
+        public override string FemaleFullName()
+        {
+            return $"{FemaleGivenName} {FamilyName}";
+        }
+
+        public override string MaleFullName()
+        {
+            return $"{MaleGivenName} {FamilyName}";
+        }
+
+        public override string ToString()
+        {
+            return $"{GivenName} {FamilyName}";
+        }
+    }
+}
